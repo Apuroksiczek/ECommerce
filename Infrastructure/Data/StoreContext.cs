1fcbf06d6fbdb43655d6323ed62967eb95404e14
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2023-08-30 18:32:18 +0200

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
